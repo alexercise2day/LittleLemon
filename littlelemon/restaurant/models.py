297e7705545edd568b2e53b487d576f31262e5f7
Alex Lim <110237573+alexercise2day@users.noreply.github.com>
2023-03-01 23:10:18 -0500

    still fiddling with models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    booking_id = models.AutoField(primary_key=True, db_column='booking_id')
+    bid = models.IntegerField(default=11)
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField(6)
     bookingdata = models.DateField()
     
 class Menu(models.Model):
-    menu_id = models.AutoField(primary_key=True, db_column='menu_id')
+    mid = models.IntegerField(default=5)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(5)
