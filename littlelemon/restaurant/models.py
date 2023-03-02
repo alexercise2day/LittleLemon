a82066f2afe873c6a26eae40dc109a7eed3722c8
Alex Lim <110237573+alexercise2day@users.noreply.github.com>
2023-03-02 10:05:31 -0500

    managed to migrate models and add entries

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    bid = models.IntegerField(default=11)
+    bid = models.IntegerField(blank=True)
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(6)
+    no_of_guests = models.IntegerField(blank=True)
     bookingdata = models.DateField()
     
 class Menu(models.Model):
-    mid = models.IntegerField(default=5)
+    mid = models.IntegerField(blank=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(5)
+    inventory = models.IntegerField(blank=True)
