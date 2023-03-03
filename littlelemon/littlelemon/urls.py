b0223fe6885ca1b7e720ce90df3394da26886639
Alex Lim <110237573+alexercise2day@users.noreply.github.com>
2023-03-03 11:40:20 -0500

    menu api in progress

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/", include('restaurant.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('restaurant/menu/items', include('restaurant.urls')),
 ]
